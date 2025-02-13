12e3b06f1a07669a7516bed21e76820a003dacfd
julian <jdrc@protonmail.com>
2025-02-13 15:33:01 -0500

    update

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
-const check = document.getElementById('toggle')
-const slider = document.querySelector('.slider')
-const basic = document.querySelector('.basic')
-const pro = document.querySelector('.pro')
-const master = document.querySelector('.master')
-let anual = true
+let checkBox = document.querySelector('#toggle');
+let Monthly = document.querySelectorAll('.price-monthly');
+let Yearly = document.querySelectorAll('.price-annually');
 
-// check.value = 'off'
+window.onload = () => {
+    checkBox.checked = false
+}
 
-console.log('init check.value =', check.value)
-// console.log(slider.value)
-
-check.addEventListener('click', (e) => {
-    anual = !anual
-    // console.log(e)
-    // console.log(anual)
-    console.log('click check.value = ', check.value)
-    if (anual) {
-        basic.textContent = '$199.99'
-        pro.textContent = '$249.99'
-        master.textContent = '$399.99'
+checkBox.addEventListener('change', function(){
+    if(checkBox.checked){
+         Yearly.forEach(price => price.style.display = 'none');
+         Monthly.forEach(price => price.style.display = 'flex');
     } else {
-        basic.textContent = '$19.99'
-        pro.textContent = '$24.99'
-        master.textContent = '$39.99'
+       Yearly.forEach(price => price.style.display = 'flex');
+         Monthly.forEach(price => price.style.display = 'none');
     }
-})
+});
